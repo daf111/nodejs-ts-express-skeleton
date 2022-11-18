62312357796682942e4a4c495cbd3332f895c8ce
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-11-18 12:36:52 -0300

    Implement returning status code

--- src/app/controllers/StatusGetController.ts
+++ src/app/controllers/StatusGetController.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
+import httpStatus from 'http-status';
 import { Controller } from './Controller';
 
 export default class StatusGetController implements Controller {
   run(req: Request, res: Response) {
-    res.json({ status: 'ok' });
+    res.status(httpStatus.OK).json({ status: 'ok' });
   }
 }
