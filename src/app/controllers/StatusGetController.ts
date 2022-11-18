642e43b61632c34c089cc52687eced5b47f3dc7a
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-11-18 12:34:18 -0300

    Implement Controller Interface

--- src/app/controllers/StatusGetController.ts
+++ src/app/controllers/StatusGetController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
+import { Controller } from './Controller';
 
-export default class StatusGetController {
+export default class StatusGetController implements Controller {
   run(req: Request, res: Response) {
     res.json({ status: 'ok' });
   }
