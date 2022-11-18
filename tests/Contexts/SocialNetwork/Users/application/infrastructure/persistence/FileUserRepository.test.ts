dba7bc64422c510dd2a22968763b1a95b0c6444b
Daniel Alejandro Fernandez <daf111@yahoo.com.ar>
2022-11-18 15:56:33 -0300

    Implement FileUserRepository integration test

---
+++ tests/Contexts/SocialNetwork/Users/application/infrastructure/persistence/FileUserRepository.test.ts
@@ -0,0 +1,11 @@
+import { User } from '../../../../../../../src/Contexts/SocialNetwork/Users/domain/User';
+import { FileUserRepository } from '../../../../../../../src/Contexts/SocialNetwork/Users/infrastructure/persistence/FileCourseRepository';
+
+describe('Save User', () => {
+  it('should have a user', async () => {
+    const repository = new FileUserRepository();
+    const user = new User('id', 'name', 'test@test.com');
+
+    await repository.save(user);
+  });
+});
